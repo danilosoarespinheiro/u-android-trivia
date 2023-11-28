69739e3f0bcc6b259e51a75e48ca5e8a6de8e953
danilosoarespinheiro <danilosoarespinheiro@gmail.com>
2023-11-28 14:31:05 -0400

    - Added Navigation to the project - Added click listener to play button

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -22,24 +22,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_title, container, false)
-        // TODO (09) Call binding.playButton.setOnClickListener and navigate to the gameFragment
-        // Use Navigation.createNavigateOnClickListener with
-        // R.id.action_titleFragment_to_gameFragment
+            inflater, R.layout.fragment_title, container, false
+        )
+        binding.playButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
         return binding.root
     }
-    // TODO (06) Add the Title Fragment to the Navigation Graph
-    // Go to the navigation.xml file and select the design tab
-    // Click the add icon with the + on it to add a new destination to the graph
-    // Select fragment_title to add this fragment to the graph as the start destination
-
-    // TODO (08) Connect the Title and Game Fragments with an Action
-    // In the navigation editor, hover over the titleFragment.  Click on the circular connection
-    // point and drag to gameFragment to create the Action
 }
